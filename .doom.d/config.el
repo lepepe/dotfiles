d00a63238ac08b448b80e606f7b4195f69f67e16
Jose Perez <jose.perez@vertilux.com>
2020-12-27 22:21:12 -0500

    Mu4e setting up multiple accounts

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -26,8 +26,32 @@
       '(("/vertilux/Inbox"    . ?i)
         ("/vertilux/sent"     . ?s)
         ("/vertilux/draft"    . ?d)
-        ("/vertilux/trash"    . ?t))
+        ("/vertilux/trash"    . ?t)
+        ("/protonmail/INBOX"  . ?p))
 )
+
+(load "~/.doom.d/email")
+
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+    (if mu4e-compose-parent-message
+      (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+        (string-match "/\\(.*?\\)/" maildir)
+        (match-string 1 maildir))
+        (completing-read (format "Compose with account: (%s) "
+           (mapconcat #'(lambda (var) (car var))
+              my-mu4e-account-alist "/"))
+           (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+           nil t nil nil (caar my-mu4e-account-alist))))
+         (account-vars (cdr (assoc account my-mu4e-account-alist))))
+  (if account-vars
+    (mapc #'(lambda (var)
+      (set (car var) (cadr var)))
+        account-vars)
+    (error "No email account found"))))
+
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
